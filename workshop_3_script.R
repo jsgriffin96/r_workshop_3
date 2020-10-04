4fcc528834d71fde3754d550b778d0dd206d630f
Jarrod <jsgriffin96@live.com>
2020-10-04 15:10:12 -0700

    added setting working directory

--- workshop_3_script.R
+++ workshop_3_script.R
@@ -2,10 +2,18 @@ install.packages('foreign')
 install.packages("haven")
 install.packages('rvest')
 install.packages('tidyverse')
+install.packages('readxl')
+install.packages('readr')
 library('foreign')
 library('haven')
 library('rvest')
 library('tidyverse')
+library('readr')
+library('readxl')
+
+#set working directory
+setwd("C:/Users/Jsgri/iCloudDrive/School/Fall 2020/AMIC/Workshops/r_workshop_3/data")
+
 
 #built in datasets
 library(help = "datasets")
@@ -28,19 +36,17 @@ View(USArrests)
 ###only use one type of importing method
 ##importing csv
 #base r
-BASE_performance.df <- read.csv('data/StudentsPerformance.csv')
+BASE_performance.df <- read.csv('StudentsPerformance.csv')
 
 #from url
 URL_performance <- read.csv("https://github.com/jsgriffin96/r_workshop_3/blob/master/data/StudentsPerformance.csv")
 
 #tidyverse readR
-READR_performance.df <- read_csv('data/StudentsPerformance.csv')
+READR_performance.df <- read_csv('StudentsPerformance.csv')
 
 
-#tidyverse
-#install.packages('readxl')
-library('readxl')
-READXL_performance.df<-read_excel('data/StudentsPerformance.xlsx')
+#tidyverse (readxl)
+READXL_performance.df<-read_excel('StudentsPerformance.xlsx')
 
 #access column name with space in it
 READXL_performance.df$`parental level of education`
@@ -53,20 +59,18 @@ PsychBike <- read.spss('PsychBike.sav', use.value.label=TRUE, to.data.frame=TRUE
 stocks <- read.delim("clipboard")
 
 ###saving data
-write.csv(mtcars, 'data/cars.csv')
+write.csv(mtcars, 'cars.csv')
 
-write.xlsx(mtcars, "data/cars.xlsx")
+write.xlsx(mtcars, "cars.xlsx")
 
 #write spss (haven)
-write_sav(mtcars, 'data/mtcars.sav')
+write_sav(mtcars, 'mtcars.sav')
 
 #export chart/plot
 hist(mtcars$mpg)
 
 
-#explain where content is from
-###Web scraping
-library('rvest')
+###Web scraping ('rvest')
 url<-"https://www.imdb.com/chart/bottom?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=4da9d9a5-d299-43f2-9c53-f0efa18182cd&pf_rd_r=Z2TFMCSJZQMJS52FPA69&pf_rd_s=right-4&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_ql_8" #need to prime with working link, #6
 webpage<-read_html(url)
 
@@ -87,4 +91,4 @@ rating <- html_text(rating_html)
 rating
 
 badMovies <- data.frame(title, year, rating)
-write.csv(badMovies, 'data/badMovies.csv')
+write.csv(badMovies, 'badMovies.csv')
